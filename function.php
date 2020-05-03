9e2b862e7eb58bda1a1509226f9b091f13df99d0
DrewoneVII <61162113+DrewoneVII@users.noreply.github.com>
2020-05-03 12:04:39 +0200

    Create function.php
    Inside the function you can use any valid PHP code, including other functions and even class declarations.
    Here we have two variables "$a" and  "$b" ===> Which we will call relatively action which we will apply to them (addition,substraction, dvivision, multiplication).

---
+++ function.php
@@ -0,0 +1,24 @@
+<?php
+ function addition ($a,$b)
+ {
+ return $a+$b;
+ }
+ function substraction ($a,$b)
+ {
+ return $a-$b;
+ }
+ function division ($a,$b)
+ {
+ if($a==0 and $b>$a)
+  return "Non value corectly";
+ else
+  {
+    return $a/$b;
+  }
+ } 
+ function multiplication($a*$b) 
+ {
+  return $a*$b;
+ }
+ 
+?>
